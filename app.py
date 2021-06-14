7d68004e99c315f855a1f40ae8c2d9c8ede86769
Athul Prakash NJ <pschosherlock@gmail.com>
2021-06-14 21:56:32 +0530

    Json is perfect. Need to add a web app for fetching and other minor features such as distrct change and vaccine alert

--- app.py
+++ app.py
@@ -3,6 +3,12 @@
 import requests
 import json
 import datetime
+from terminaltables import AsciiTable
+
+tableData = []
+
+table = AsciiTable(tableData)
+tableData.append(['Vaccine', 'Address', 'Min-Age', 'Total', 'Dose1', 'Dose2' ])
 
 today = datetime.date.today().strftime('%d-%m-%y') # For date time format
 
@@ -10,11 +16,23 @@ headers = {
     "accept" : "application/json",
     "Accept-Language" : "hi_IN"
 }
+districtID = "297"
 
-url = f"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=297&date={today}"
-
-
+url = f"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id={districtID}&date={today}"
 data = requests.get(url, headers)
 
-print(data.text)
+# dump = json.dumps(data.text, indent=4) # For coverting python to json
+jsonData = json.loads(data.text) # for loading data as json
 
+for i in range(len(jsonData['sessions'])):
+    centerName = jsonData['sessions'][i]['name']
+    vaccineName = jsonData['sessions'][i]['vaccine']
+    age_limit = jsonData['sessions'][i]['min_age_limit']
+    totalCapacity = jsonData['sessions'][i]['available_capacity']
+    dose1 = jsonData['sessions'][i]['available_capacity_dose1']
+    dose2 = jsonData['sessions'][i]['available_capacity_dose2']
+    # print(f'{centerName} {vaccineName} {age_limit} {totalCapacity} {dose1} {dose2}')
+    tableData.append([vaccineName, centerName, age_limit, totalCapacity, dose1, dose2])
+
+
+print(table.table)
