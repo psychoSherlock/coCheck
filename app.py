0e0f27f707a8b713cbb2e5fa2e687010416074c1
Athul Prakash NJ <pschosherlock@gmail.com>
2021-06-15 18:10:58 +0530

    Added everything related to flask. Need to add more css, other infos and js Also need to add search feature and others

--- app.py
+++ app.py
@@ -1,45 +1,19 @@
-# Author: Athul Prakash 
+from flask import Flask, render_template
+from vAPI import cowin
 
-import requests
-import json
-import datetime
-from terminaltables import AsciiTable
-
-tableData = []
-
-table = AsciiTable(tableData)
-tableData.append(['Vaccine', 'Address', 'Min-Age', 'Total', 'Dose1', 'Dose2' ])
-
-today = datetime.date.today().strftime('%d-%m-%y') # For date time format
-
-headers = {
-    "accept" : "application/json",
-    "Accept-Language" : "hi_IN"
-}
 kannurID = 297
 
+kannur = cowin(kannurID)
+data = kannur.check()
 
-class cowin:
-	def __init__(self, address):
-		self.address = address
-	def check(self):
+sessionsLen = len(data['sessions'])
 
-		url = f"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id={self.address}&date={today}"
-		data = requests.get(url, headers)
+app = Flask(__name__)
 
-		# dump = json.dumps(data.text, indent=4) # For coverting python to json
-		jsonData = json.loads(data.text) # for loading data as json
-		
-		return jsonData
-"""
-		for i in range(len(jsonData['sessions'])):
-		    centerName = jsonData['sessions'][i]['name']
-		    vaccineName = jsonData['sessions'][i]['vaccine']
-		    age_limit = jsonData['sessions'][i]['min_age_limit']
-		    totalCapacity = jsonData['sessions'][i]['available_capacity']
-		    dose1 = jsonData['sessions'][i]['available_capacity_dose1']
-		    dose2 = jsonData['sessions'][i]['available_capacity_dose2']
-		    # print(f'{centerName} {vaccineName} {age_limit} {totalCapacity} {dose1} {dose2}')
-		    tableData.append([vaccineName, centerName, age_limit, totalCapacity, dose1, dose2])
-		return centerName
-"""
+@app.route('/')
+def index():
+    return render_template('index.html', jsonData=data, items=sessionsLen)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
