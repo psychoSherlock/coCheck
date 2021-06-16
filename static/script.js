0270053051e636cbce939f178f9c2324dd12bb90
Athul Prakash NJ <pschosherlock@gmail.com>
2021-06-16 18:52:52 +0530

    Javascript for dates. Need to add more css Enchance that button Enchance that date-form colors and color palette

---
+++ static/script.js
@@ -0,0 +1,19 @@
+
+// For reversing value of date
+function reverseDate(date){
+    var before = document.getElementById(date).value;
+    return before.split('-').reverse().join('-');
+}
+
+
+function searchDate(){
+    date = reverseDate('date-field')
+    console.log(date)
+    if (date) {
+        window.location = '/date/' + date
+    }
+    else{
+        alert("Please specify a date")
+    }
+    
+}
