72d9add04d1009c346698bf9299a91195660937c
Athul Prakash NJ <pschosherlock@gmail.com>
2021-06-27 22:20:41 +0530

    😸 At last, everything is set. I need to maintain some css for mobiles and a custom alert box that I've disabled But lets set it out for production and see its performance

--- README.md
+++ README.md
@@ -5,13 +5,13 @@
 * Footer for landing page and other css for elements
 * Debug the api [Done]
 * 404 and 500 [Done]
-* District mappings on land page
+* District mappings on land page [Done]
 * Different pages for district and pincodes [No need]
 * Add error codes [Done]
 * List redirection js on land page [Done]
-* Header on every page.
-* CSS FOR options
-* Custom alert boxes
+* Header on every page. [Done]
+* CSS FOR options [No need]
+* Custom alert boxes[Done]
 * Fix css on android {
     1. The bottom footer
     2. The alert box margin-top
--- static/land.js
+++ static/land.js
@@ -13,65 +13,65 @@ function reverseDate(date) {
 
 // For alert messages
 
-var msgBox = document.getElementById("msg-box");
-var holder = document.getElementById("msg-holder");
+// var msgBox = document.getElementById("msg-box");
+// var holder = document.getElementById("msg-holder");
 
-function warn(msg, color) {
-  msgBox.style.animation = "slideIn 1s";
-  msgBox.style.display = "block";
-  msgBox.style.right = "0";
-  msgBox.style.opacity = "1";
-  holder.innerHTML = msg;
-  msgBox.style.backgroundColor = "var(" + color + ")";
-  clean();
-}
+// function warn(msg, color) {
+//   msgBox.style.animation = "slideIn 1s";
+//   msgBox.style.display = "block";
+//   msgBox.style.right = "0";
+//   msgBox.style.opacity = "initial";
+//   holder.innerHTML = msg;
+//   msgBox.style.backgroundColor = "var(" + color + ")";
+//   clean();
+// }
 
-function cleaner() {
-  msgBox.style.opacity = "0";
-  msgBox.style.animation = "slideOut 2s";
-}
+// function cleaner() {
+//   msgBox.style.opacity = "0";
+//   msgBox.style.animation = "slideOut 2s";
+// }
 
-function clean() {
-  setTimeout(function () {
-    cleaner();
-  }, 4000);
-}
-
-
-
-function search() {
-  // Validate inputs
-  date = reverseDate("date-field");
-
-  if (date) {
-    // Checking for date presence
-    if (method.value == "dis-opt") {
-      // iF district
+// function clean() {
+  //   setTimeout(function () {
+    //     cleaner();
+    //   }, 4000);
+    // }
+    
+    
+    
+    function search() {
+      // Validate inputs
+      date = reverseDate("date-field");
+      
+      if (date) {
+        // Checking for date presence
+        if (method.value == "dis-opt") {
+          // iF district
       var distr = districtField.value;
-
+      
       if (is_valid_datalist_value("district-id", districtField.value)) {
-        warn("Please wait while the server fetch data for " + distr , '--success')
+        alert("Please wait while the server fetch data for " + distr)
         window.location = "/district/id=" + distr + "&date=" + date;
       }
-
+      
       // Checking if district in input valid or not
       else {
-        warn("Not a valid option", "--danger");
+        alert("Please type in a district name, and choose the appropriate value from the option. Vaccine are checked by district ID, not name");
       }
     } else if (method.value == "pin-opt") {
       var pincode = pinField.value;
-
+      
       if (pincode) {
         // If pincode
-        warn("Please wait while the server fetch data for " + pincode , '--success')
+        alert("Please wait while the server fetch data for " + pincode)
         window.location = "/pincode/pin=" + pincode + "&date=" + date;
       } else {
-        warn("No Pin code specified? Trying to trick my website :(", "--warn");
+        alert("No Pin code specified? Trying to trick my website :(");
       }
     }
   } else {
     // For no date
-    warn("Please specify a date", "--warn");
+    alert("Please specify a date");
   }
 }
 
@@ -79,28 +79,57 @@ function is_valid_datalist_value(idDataList, inputValue) {
   // Validate options
   var option = document.querySelector(
     "#" + idDataList + " option[value='" + inputValue + "']"
-  );
-  if (option != null) {
-    return option.value.length > 0;
+    );
+    if (option != null) {
+      return option.value.length > 0;
+    }
+    return false;
   }
-  return false;
-}
 
-function getMethod() {
-  if (method.value == "dis-opt") {
-    pinField.style.display = "none";
-    districtField.style.display = "inline";
+  function getMethod() {
+    if (method.value == "dis-opt") {
+      pinField.style.display = "none";
+      districtField.style.display = "inline";
   } else if (method.value == "pin-opt") {
     pinField.style.display = "inline";
     districtField.style.display = "none";
   }
 }
 
+
+// Hide footer
+var footer = document.getElementById('footer')
+function forMobile(size) {
+  if (size.matches) { // If media query matches
+    footer.style.display = 'none'
+  } else {
+    footer.style.display = 'block'
+  }
+}
+
+const mobile = window.matchMedia("(max-width: 480px)")
+
+if (mobile.matches){
+  console.log(window.innerWidth)
+  console.log("Matches")
+
+  var inputFileds = document.getElementsByClassName('input-field')
+  districtField.addEventListener('click', function(){
+    footer.style.display = 'none'
+    hided = true
+  })
+  pinField.addEventListener('click', function(){
+    footer.style.display = 'none'
+  })
+}
+
+
+
+
 function load(){
   getMethod()
   districtField.value = ""
   pinField.value=""
 }
-
 
 window.onload = load()
